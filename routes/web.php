2b8f36bb06237670b4ec641efa1ef671abea294c
Michael <michael.rolle@hotmail.com>
2020-08-25 15:48:36 +0200

    route middleware

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
 
-Route::get('{order}/downloadPDF', 'OrderController@downloadPDF')->name('downloadPDF');
 
-Route::resources([
-    'clients' => 'ClientController',
-    'drivers' => 'DriverController',
-    'locations' => 'LocationController',
-    'orders' => 'OrderController',
-    'products' => 'ProductController',
-]);
+
+Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
+    Route::get('{order}/downloadPDF', 'OrderController@downloadPDF')->name('downloadPDF');
+    Route::resources([
+        'clients' => 'ClientController',
+        'drivers' => 'DriverController',
+        'locations' => 'LocationController',
+        'orders' => 'OrderController',
+        'products' => 'ProductController',
+    ]);
+});
+
 
 
 Auth::routes();
